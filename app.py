469373ab98400e718e78f9d5704c53bc1efc05b3
David Goldberg <goodwordachemy@gmail.com>
2017-10-03 16:50:46 -0700

    yet another debugging print statement

--- app.py
+++ app.py
@@ -30,9 +30,9 @@ def _api_get_user_events_list():
     for event in user_events_list:
         event_date = datetime.strptime(event['start']['date'], DATE_PICKER_FORMAT)
         if event_date >= start_date and event_date <= end_date:
-            print(event)
             filtered_user_events_list.append(event)
 
+    print jsonify(filtered_user_events_list)
     return jsonify(filtered_user_events_list)
 
 
